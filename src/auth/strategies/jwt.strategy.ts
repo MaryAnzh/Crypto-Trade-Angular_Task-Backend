1bc3eb21cdd05eb82f357d48a9c416ee22d330bd
MaryAnzh <maryang@mail.ru>
2026-05-24 17:59:26 +0300

    feat: add JwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,1 +1,15 @@
-export class JwtStrategy {}
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
+
+    async validate(payload: JwtPayload) {
+        return { id: payload.sub, email: payload.email };
+    }
+}
