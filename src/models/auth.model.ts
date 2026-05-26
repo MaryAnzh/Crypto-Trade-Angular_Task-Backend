79a8bb0b7783cb7e2e8a1a730c386087e4ac4af8
MaryAnzh <maryang@mail.ru>
2026-05-26 17:06:58 +0300

    feat: add field user in AuthResponse

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,5 +1,12 @@
-export interface JwtPayload {
+import { UserSafe } from "./user.model";
+
+export type JwtPayload = {
     /** uuid  */
     sub: string;
     email: string;
+}
+
+export type AuthResponse = {
+    accessToken: string;
+    user: UserSafe;
 }
